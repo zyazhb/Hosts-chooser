4c49e879d6d78164502b33e183121fc15d1b5f33
zyazhb <zyazhb@yahoo.com>
2020-06-19 19:45:20 +0800

    Add README.MD

--- dns.py
+++ dns.py
@@ -19,22 +19,24 @@ def initArguments():
         description="Prevent domains from dns polution.")
     parser.add_argument("-t", help="Your target domain", type=str)
     parser.add_argument("-r", help="Read a domain list", type=str)
-    parser.add_argument("--clean", help="Speed test", action="store_true")
+    parser.add_argument(
+        "--clean", help="Speed test and sort", action="store_true")
     parser.add_argument("--area", help="Choose area in china asia europe africa oceania north_america south_america",
-                        choices=["china", "asia", "europe", "africa", "oceania", "north_america", "south_america"])
+                        choices=["china", "asia", "europe", "africa", "oceania", "north_america", "south_america", "debug"], type=str, default="north_america", nargs='?')
     args = parser.parse_args()
     return args
 
 
 def run(domain, headers, area):
     # Encrypt!
-    '''
-    r = requests.get("https://en.ipip.net/dns.php?a=dig&host=" +
-                     domain+"&area%5B%5D="+area, headers=headers)
-    iplist = re.findall("\\d+\\.\\d+\\.\\d+\\.\\d+", r.text)
-    '''
-    iplist = ['220.181.38.148', '39.156.69.79', '210.23.129.34', '210.23.129.34', '220.181.38.148', '39.156.69.79', '202.108.22.220', '220.181.33.31', '112.80.248.64', '14.215.178.80', '180.76.76.92', '210.23.129.34', '210.23.129.34', '39.156.69.79', '220.181.38.148', '203.12.160.35', '203.12.160.35', '39.156.69.79', '220.181.38.148',
-              '202.108.22.220', '220.181.33.31', '112.80.248.64', '14.215.178.80', '180.76.76.92', '203.12.160.35', '203.12.160.35', '220.181.38.148', '39.156.69.79', '61.8.0.113', '61.8.0.113', '220.181.38.148', '39.156.69.79', '202.108.22.220', '220.181.33.31', '112.80.248.64', '14.215.178.80', '180.76.76.92', '61.8.0.113', '61.8.0.113']
+    if area == "debug":
+        iplist = ['220.181.38.148', '39.156.69.79', '210.23.129.34', '210.23.129.34', '220.181.38.148', '39.156.69.79', '202.108.22.220', '220.181.33.31', '112.80.248.64', '14.215.178.80', '180.76.76.92', '210.23.129.34', '210.23.129.34', '39.156.69.79', '220.181.38.148', '203.12.160.35', '203.12.160.35', '39.156.69.79', '220.181.38.148',
+                  '202.108.22.220', '220.181.33.31', '112.80.248.64', '14.215.178.80', '180.76.76.92', '203.12.160.35', '203.12.160.35', '220.181.38.148', '39.156.69.79', '61.8.0.113', '61.8.0.113', '220.181.38.148', '39.156.69.79', '202.108.22.220', '220.181.33.31', '112.80.248.64', '14.215.178.80', '180.76.76.92', '61.8.0.113', '61.8.0.113']
+    else:
+        r = requests.get("https://en.ipip.net/dns.php?a=dig&host=" +
+                         domain+"&area%5B%5D="+area, headers=headers)
+        iplist = re.findall("\\d+\\.\\d+\\.\\d+\\.\\d+", r.text)
+        print("[+]Got domain! \n" + str(iplist))
     return domain, iplist
 
 
@@ -47,6 +49,7 @@ def ping_host(ip):
 
 def clean(iplist):
     # TODO
+    print("[+]Cleaning...")
     ip_dic = dict()
     for ip in iplist:
         ip_dic[ip] = ping_host(ip)
