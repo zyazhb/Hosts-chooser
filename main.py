55a78c178b0d06813e6dddfc40b5a911df195c07
zyazhb <zyazhb@yahoo.com>
2020-06-20 09:27:47 +0800

    Try to add multi-threads

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import requests
-import re
+#! /usr/bin/env python3
 from fake_useragent import UserAgent
 import argparse
 import myutils
@@ -14,44 +13,32 @@ def init():
 def initArguments():
     parser = argparse.ArgumentParser(
         description="Prevent domains from dns polution.")
-    parser.add_argument("-t", help="Your target domain", type=str)
+    parser.add_argument("-t", "--target", help="Your target domain", type=str)
     parser.add_argument("-r", help="Read a domain list", type=str)
     parser.add_argument(
         "--clean", help="Speed test and sort", action="store_true")
     parser.add_argument("--area", help="Choose area in china asia europe africa oceania north_america south_america",
                         choices=["china", "asia", "europe", "africa", "oceania", "north_america", "south_america", "debug"], type=str, default="north_america", nargs='?')
+    parser.add_argument(
+        "--thread", help="Run num of threads,default is 3", type=int, default=3)
     args = parser.parse_args()
-    return args
-
-
-def run_core(domain, headers, area):
-    # Encrypt!
-    if area == "debug":
-        iplist = ['220.181.38.148', '39.156.69.79', '210.23.129.34', '210.23.129.34', '220.181.38.148', '39.156.69.79', '202.108.22.220', '220.181.33.31', '112.80.248.64', '14.215.178.80', '180.76.76.92', '210.23.129.34', '210.23.129.34', '39.156.69.79', '220.181.38.148', '203.12.160.35', '203.12.160.35', '39.156.69.79', '220.181.38.148',
-                  '202.108.22.220', '220.181.33.31', '112.80.248.64', '14.215.178.80', '180.76.76.92', '203.12.160.35', '203.12.160.35', '220.181.38.148', '39.156.69.79', '61.8.0.113', '61.8.0.113', '220.181.38.148', '39.156.69.79', '202.108.22.220', '220.181.33.31', '112.80.248.64', '14.215.178.80', '180.76.76.92', '61.8.0.113', '61.8.0.113']
-    else:
-        r = requests.get("https://en.ipip.net/dns.php?a=dig&host=" +
-                         domain+"&area%5B%5D="+area, headers=headers)
-        iplist = re.findall("\\d+\\.\\d+\\.\\d+\\.\\d+", r.text)
-        print("[+]Got domain! \n" + str(iplist))
-    return domain, iplist
+    return parser, args
 
 
 def main():
     headers = init()
-    args = initArguments()
-    if args.t:
-        domain, iplist = run_core(args.t, headers, args.area)
+    parser, args = initArguments()
+    if args.target:
+        domain, iplist = myutils.run_core(args.target, headers, args.area)
     elif args.r:
         for domain in open(args.r):
-            domain, iplist = run_core(domain, headers, args.area)
+            domain, iplist = myutils.run_core(domain, headers, args.area)
     else:
-        iplist = ""
+        parser.print_help()
 
     if args.clean:
+        #myutils.thread_work(args.thread, myutils.clean, iplist)
         myutils.output_dic(domain, myutils.clean(iplist))
-    else:
-        print(iplist)
 
 
 if __name__ == "__main__":
--- myutils.py
+++ myutils.py
@@ -1,20 +1,39 @@
+import requests
+import re
 from ping3 import ping
+
+
+def run_core(domain, headers, area):
+    # Encrypt!
+    if area == "debug":
+        iplist = ['220.181.38.148', '39.156.69.79', '210.23.129.34', '210.23.129.34', '220.181.38.148', '39.156.69.79', '202.108.22.220', '220.181.33.31', '112.80.248.64', '14.215.178.80', '180.76.76.92', '210.23.129.34', '210.23.129.34', '39.156.69.79', '220.181.38.148', '203.12.160.35', '203.12.160.35', '39.156.69.79', '220.181.38.148',
+                  '202.108.22.220', '220.181.33.31', '112.80.248.64', '14.215.178.80', '180.76.76.92', '203.12.160.35', '203.12.160.35', '220.181.38.148', '39.156.69.79', '61.8.0.113', '61.8.0.113', '220.181.38.148', '39.156.69.79', '202.108.22.220', '220.181.33.31', '112.80.248.64', '14.215.178.80', '180.76.76.92', '61.8.0.113', '61.8.0.113']
+    else:
+        r = requests.get("https://en.ipip.net/dns.php?a=dig&host=" +
+                         domain+"&area%5B%5D="+area, headers=headers)
+        iplist = re.findall("\\d+\\.\\d+\\.\\d+\\.\\d+", r.text)
+        print("[+]Got domain! \n" + str(iplist))
+    return domain, iplist
 
 
 def ping_host(ip):
     response = ping(ip)
     if response is not None:
         delay = int(response * 1000)
+        #print("[+]ping "+str(ip))
         return delay
+
 
 def clean(iplist):
     # TODO
     print("[+]Cleaning...")
     ip_dic = dict()
     for ip in iplist:
+        # 创建线程
         ip_dic[ip] = ping_host(ip)
     ip_dic = sorted(ip_dic.items(), key=lambda kv: (kv[1], kv[0]))
     return(ip_dic)
+
 
 def output_dic(domain, ip_dic):
     for ip, delay in ip_dic:
