6b6ae7a6569dfc16f1a7246b5b47bb0ae9d39140
ZYA <zyazhb@yahoo.com>
2020-07-05 20:51:46 +0800

    Moudled multi (#17)
    有几个小地方没处理好 还有没用的引入

--- myutils.py
+++ myutils.py
@@ -1,24 +1,9 @@
-import subprocess
-import time
 import requests
 import re
 from ping3 import ping
 import multi
 from prettytable import PrettyTable
-from threading import Thread, Lock
 
-ipll = []
-class DIG(Thread):
-    def __init__(self, ip, domain):
-        Thread.__init__(self)
-        self.ip = ip
-        self.domain = domain
-
-    def run(self):
-        response = subprocess.check_output("dig @{0} {1}".format(self.ip, self.domain), shell=True)
-        if r";; connection timed out; no servers could be reached" not in str(response):
-            ipll.append(self.ip)
-            
 
 def run_core(domain, area):
     # Encrypt!
@@ -26,18 +11,7 @@ def run_core(domain, area):
         iplist = ['220.181.38.148', '39.156.69.79', '210.23.129.34', '210.23.129.34', '220.181.38.148', '39.156.69.79', '202.108.22.220', '220.181.33.31', '112.80.248.64', '14.215.178.80', '180.76.76.92', '210.23.129.34', '210.23.129.34', '39.156.69.79', '220.181.38.148', '203.12.160.35', '203.12.160.35', '39.156.69.79', '220.181.38.148',
                   '202.108.22.220', '220.181.33.31', '112.80.248.64', '14.215.178.80', '180.76.76.92', '203.12.160.35', '203.12.160.35', '220.181.38.148', '39.156.69.79', '61.8.0.113', '61.8.0.113', '220.181.38.148', '39.156.69.79', '202.108.22.220', '220.181.33.31', '112.80.248.64', '14.215.178.80', '180.76.76.92', '61.8.0.113', '61.8.0.113']
     else:
-        with open("dns.txt", "r") as f_dns:
-            iplist_unsolve = [x.replace('\n', '') for x in f_dns.readlines() ]
-
-        # 多线程dig
-        T_thread = []
-        for i in iplist_unsolve:
-            T_thread.append(DIG(i, domain=domain))
-        for i in range(len(T_thread)):
-            T_thread[i].start()
-        time.sleep(3)
-
-        print("[+]Got domain! \n" + str(ipll))
+        domain, ipll = multi.multi_local_dns(domain)
     return domain, ipll
 
 
