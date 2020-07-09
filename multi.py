0344f48fee9dcb016ffd66a27387e9ff83c108ad
root <root@404-NOT-FOUND.localdomain>
2020-07-09 10:05:37 +0800

    Add windows support

--- multi.py
+++ multi.py
@@ -2,10 +2,11 @@
 # _*_coding:utf-8_*_
 #
 from threading import Thread
+import subprocess
 from ping3 import ping
 import time
-import subprocess
 import re
+import sys
 
 ip_dic = dict()
 
@@ -29,12 +30,11 @@ def multi_ping(iplist):
         T_thread.append(PING(i))
     for i in range(len(T_thread)):
         T_thread[i].start()
-    time.sleep(3)
+    time.sleep(5)
     return ip_dic
 
 
 iplist = []
-
 
 class DIG(Thread):
     def __init__(self, ip, domain):
@@ -43,19 +43,31 @@ class DIG(Thread):
         self.domain = domain
 
     def run(self):
+        runtimePlatform = sys.platform
+
         # response = subprocess.check_output(
         #     "dig @{0} {1} +short".format(self.ip, self.domain), shell=True)
+        if runtimePlatform == "linux":
+            proc = subprocess.Popen(
+                "dig @{0} {1} +short".format(self.ip, self.domain), shell=True, close_fds=True, stdout=subprocess.PIPE)
+        else:
+            proc = subprocess.Popen(
+                "nslookup {0} {1}".format(self.domain, self.ip), shell=True, stdout=subprocess.PIPE)
 
-        proc = subprocess.Popen(
-            "dig @{0} {1} +short".format(self.ip, self.domain), shell=True, stdout=subprocess.PIPE)
+        # time.sleep(1.5)
         response = proc.stdout.read()
         proc.kill()
-        if r";; connection timed out; no servers could be reached" not in str(response) and \
-            response.decode() != '':
-            ans = str(response).split("\\n")[1]
-            if re.search("\\d+\\.\\d+\\.\\d+\\.\\d+", ans) != None:
-                iplist.append(ans)
 
+        if runtimePlatform == "linux":
+            if ";; connection timed out; no servers could be reached" not in response.decode():
+                ans = re.findall("\\d+\\.\\d+\\.\\d+\\.\\d+", str(response.decode()))
+                if ans != []:
+                    iplist.extend(ans)
+        else:
+            # TODO 完善输出机制
+            ans = re.findall("\\d+\\.\\d+\\.\\d+\\.\\d+", str(response))
+            if ans != []:
+                iplist.extend(ans[1:])
 
 def multi_local_dns(domain):
     with open("dns.txt", "r") as f_dns:
@@ -67,7 +79,7 @@ def multi_local_dns(domain):
             T_thread.append(DIG(i, domain=domain))
         for i in range(len(T_thread)):
             T_thread[i].start()
-        time.sleep(3)
+        time.sleep(15)
 
         # 去重
         iplist_change = list(set(iplist))
