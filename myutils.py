a96fc1452b94a7e1e762930fb9c13035b2f586cf
root <root@404-NOT-FOUND.localdomain>
2020-07-13 17:47:35 +0800

    change Threading to Async

--- myutils.py
+++ myutils.py
@@ -11,8 +11,9 @@ def run_core(domain, area):
         iplist = ['220.181.38.148', '39.156.69.79', '210.23.129.34', '210.23.129.34', '220.181.38.148', '39.156.69.79', '202.108.22.220', '220.181.33.31', '112.80.248.64', '14.215.178.80', '180.76.76.92', '210.23.129.34', '210.23.129.34', '39.156.69.79', '220.181.38.148', '203.12.160.35', '203.12.160.35', '39.156.69.79', '220.181.38.148',
                   '202.108.22.220', '220.181.33.31', '112.80.248.64', '14.215.178.80', '180.76.76.92', '203.12.160.35', '203.12.160.35', '220.181.38.148', '39.156.69.79', '61.8.0.113', '61.8.0.113', '220.181.38.148', '39.156.69.79', '202.108.22.220', '220.181.33.31', '112.80.248.64', '14.215.178.80', '180.76.76.92', '61.8.0.113', '61.8.0.113']
     else:
-        domain, iplist = multi.multi_local_dns(domain)
-    return domain, iplist
+        ipdict = multi.multi_local_dns(domain)
+        print("[+]Got domain! \n" + str(list(ipdict[1].keys())))
+    return domain, ipdict
 
 
 def run_remote_core(domain, area):
@@ -37,19 +38,19 @@ def run_remote_core(domain, area):
         raise domainError
 
 
-def clean(iplist):
-    print("[+]Start Cleaning...")
-    ip_dic = multi.multi_ping(iplist)
-    ip_dic = sorted(ip_dic.items(), key=lambda kv: (kv[1], kv[0]))
-    return(ip_dic)
+# def clean(iplist):
+#     print("[+]Start Cleaning...")
+#     ip_dic = multi.multi_ping(iplist)
+#     ip_dic = sorted(ip_dic.items(), key=lambda kv: (kv[1], kv[0]))
+#     return(ip_dic)
 
 
 def output_dic(domain, ip_dic):
     print("[+]Output:")
 
-    table = PrettyTable(["Domain", "ip", "delay"])
+    table = PrettyTable(["Domain", "ip", "delay(/s)"])
     table.align["Domain"] = "l"
-    for ip, delay in ip_dic:
+    for ip, delay in ip_dic.items():
         # print(str(domain) + "    " + str(ip) + "    " + str(delay))
         table.add_row([domain, ip, delay])
 
