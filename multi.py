573e7c88267c645196773abb284d2d9b5d3c5aee
root <root@404-NOT-FOUND.localdomain>
2020-07-20 15:58:50 +0800

    change threading to async

--- multi.py
+++ multi.py
@@ -1,13 +1,28 @@
 import asyncio
 import aiohttp
+import uvloop
 
 import time
 import subprocess
 import re
-import gc
 
 
-now = lambda : time.time()
+class MyConnector(aiohttp.TCPConnector):
+    def __init__(self, ip):
+        self.__ip = ip
+        super().__init__()
+
+    async def _resolve_host(
+        self, host: str, port: int,
+        traces: None = None,
+    ):
+        return [{
+            'hostname': host, 'host': self.__ip, 'port': port,
+            'family': self._family, 'proto': 0, 'flags': 0,
+        }]
+
+
+def now(): return time.time()
 
 
 with open("dns.txt") as f:
@@ -16,7 +31,6 @@ dns_list = [i.strip() for i in a]
 
 ip_list = []
 time_list = {}
-
 
 async def run(cmd):
     proc = await asyncio.create_subprocess_shell(
@@ -35,11 +49,13 @@ async def run(cmd):
 
 async def test_doamin_ip(ip):
     st = now()
-    async with aiohttp.ClientSession() as client:
-        async with client.get("https://{0}".format(ip), ssl=False, timeout=59) as resp:
-            if resp.status == 200:
-                time_list[ip] = now() - st
-
+    try:
+        async with aiohttp.ClientSession(connector=MyConnector(ip), timeout=aiohttp.ClientTimeout(total=10)) as client:
+            async with client.get("https://{0}".format(ip), ssl=False, timeout=10) as resp:
+                if resp.status == 200:
+                    time_list[ip] = now() - st
+    except asyncio.TimeoutError:
+        pass
 
 async def dns_test(domain):
     task_list = [asyncio.create_task(
@@ -49,8 +65,15 @@ async def dns_test(domain):
     task_speed = [asyncio.create_task(test_doamin_ip(ip))
                   for ip in set(ip_list)]
     done, pending = await asyncio.wait(task_speed)
+<<<<<<< HEAD
 
+=======
+>>>>>>> change threading to async
 
 def multi_local_dns(domain):
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
+    start = now()
     asyncio.run(dns_test(domain))
+    print("Time: ", now() - start)
     return domain, time_list
